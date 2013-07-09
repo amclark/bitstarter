de32c18cac57ba91a445430e29e72824340ba849
Andrew Clark <amclark7@gmail.com>
2013-07-08 23:10:53 -0700

    Added paper.js

--- web.js
+++ web.js
@@ -3,9 +3,19 @@ var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
+app.get('/', function(req, res) {
   var buf = fs.readFileSync('index.html');
-  response.send(buf.toString('utf8'));
+  res.send(buf.toString('utf8'));
+});
+
+app.get('/js/:file', function(req, res) {
+    var file = 'js/' + req.params.file;
+    if (!req.params.file || !fs.existsSync(file)) {
+        res.send(404, 'Not found');
+    } else {
+        var buf = fs.readFileSync(file);
+        res.send(buf.toString('utf8'));
+    }
 });
 
 var port = process.env.PORT || 5000;
