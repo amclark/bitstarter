adf3bc25ad83da2bddaf9d5a53583949da30ef7c
Andrew Clark <amclark7@gmail.com>
2013-07-11 21:49:42 -0700

    Modifications to web.js, added a paper example

--- web.js
+++ web.js
@@ -1,21 +1,30 @@
 var express = require('express');
 var fs = require('fs');
+var util = require('util');
 
 var app = express.createServer(express.logger());
 
+function send_file(res, file) {
+    if (!file) return res.send('Not found', 404);
+    return fs.exists(file, function(exists) {
+        if (!exists) return res.send('Not found', 404);
+        return fs.readFile(file, function(err, data) {
+            if (err) throw err;
+            return res.send(data.toString('utf8'));
+        });
+    });
+}
+
 app.get('/', function(req, res) {
-  var buf = fs.readFileSync('index.html');
-  res.send(buf.toString('utf8'));
+    return send_file(res, 'index.html');
 });
 
-app.get('/js/:file', function(req, res) {
-    var file = 'js/' + req.params.file;
-    if (!req.params.file || !fs.existsSync(file)) {
-        res.send(404, 'Not found');
-    } else {
-        var buf = fs.readFileSync(file);
-        res.send(buf.toString('utf8'));
-    }
+app.get('/:file', function(req, res) {
+    return send_file(res, req.params.file);
+});
+
+app.get('/:dir/:file', function(req, res) {
+    return send_file(res, req.params.dir + '/' + req.params.file);
 });
 
 var port = process.env.PORT || 5000;
