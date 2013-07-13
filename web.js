f4be9471af1f449d12c83571301f3dce14596cdf
Andrew Clark <amclark7@gmail.com>
2013-07-13 08:21:44 -0700

    Updated express and changed how static files are served

--- web.js
+++ web.js
@@ -2,30 +2,9 @@ var express = require('express');
 var fs = require('fs');
 var util = require('util');
 
-var app = express.createServer(express.logger());
+var app = express();
 
-function send_file(res, file) {
-    if (!file) return res.send('Not found', 404);
-    return fs.exists(file, function(exists) {
-        if (!exists) return res.send('Not found', 404);
-        return fs.readFile(file, function(err, data) {
-            if (err) throw err;
-            return res.send(data.toString('utf8'));
-        });
-    });
-}
-
-app.get('/', function(req, res) {
-    return send_file(res, 'index.html');
-});
-
-app.get('/:file', function(req, res) {
-    return send_file(res, req.params.file);
-});
-
-app.get('/:dir/:file', function(req, res) {
-    return send_file(res, req.params.dir + '/' + req.params.file);
-});
+app.use(express.static(__dirname + '/public'));
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
